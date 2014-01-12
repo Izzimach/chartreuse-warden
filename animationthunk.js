85b61a1d6d725fa46d74c5bd18b153bdf9ae8d71
Gary Haussmann <gjhaussmann@gmail.com>
2014-01-11 22:26:38 -0700

    Added animation map to animation thunk.

--- animationthunk.js
+++ animationthunk.js
@@ -4,11 +4,22 @@
 // You can specify a default animation to play, then fire off one-shot animations.
 // When the one-shot animations finish this will switch back to the default animation.
 //
+
+//
+// a JSON object mapping from common animation names like 'idle','walk' to the actual
+// animation data. animation data is another map with keys 'name','playspeed', and 'duration'
+//
+pc.script.attribute('animationmap','string', "{idle:{name:'idle_anim', playspeed:1, duration:1}}");
+
 pc.script.create('animationthunk', function (context) {
 
     var AnimationThunk = function (entity) {
         this.entity = entity;
         this.shapes = [];
+        this.defaultanimation = "";
+        this.currentoneshot = "";
+        this.isplayingoneshot = false;
+        this.oneshottimeleft = 0;
     };
 
     AnimationThunk.prototype = {
@@ -22,6 +33,19 @@ pc.script.create('animationthunk', function (context) {
 
         getComponentReference: function() {
             return this;
+        },
+
+        setDefaultAnimation: function(animationname) {
+            if (this.defaultanimation !== animationname) {
+                this.defaultanimation = animationname;
+                var playdata = this.animationmap[animationname];
+
+                if (!this.isplayingoneshot && playdata) {
+                    this.entity.animation.loop = true;
+                    this.entity.animation.speed = playdata.playspeed;
+                    this.entity.animation.play(playdata.name);
+                }
+            }
         },
 
         playOneShot: function(animationname) {
