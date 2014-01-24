86252a5005d5098e9cbe9c84f1b8a7ef753584f4
Gary Haussmann <gjhaussmann@gmail.com>
2014-01-24 14:09:21 -0700

    Moved world map placement code chunks into their own methods.

--- worldmap.js
+++ worldmap.js
@@ -15,8 +15,6 @@ pc.script.create('worldmap', function (context) {
         },
 
         buildmap: function() {
-            var basehex = this.entity.findByName('basichex');
-            var basetree = this.entity.findByName('basictree');
             var baserock = this.entity.findByName('rock');
             var marker1 = this.entity.findByName('marker1');
             var marker2 = this.entity.findByName('marker2');
@@ -24,17 +22,68 @@ pc.script.create('worldmap', function (context) {
             var worldhexes = chartreusewarden.generatemap(hexsize, numhexes);
             var obstacledata = chartreusewarden.generateobstacles(worldhexes);
 
-            var hexes = _.values(worldhexes.mapdata);
             var entity = this.entity;
+            var hexes = _.values(worldhexes.mapdata);
 
-            // place hexes
+            this.placehexes(worldhexes);
+
+            this.placewalls(worldhexes);
+
+            // place notations for the hexes (used for debugging)
+            _.forEach(hexes, function(hex) {
+                // mark spot with a region tag of some sort
+                if (_.some(hex.tags, function(tag) {return tag === 'reachable';})) {
+                    pc.log.write('reachable hex at ' + hex.hexcoord.toString() + ', world coord ' + hex.worldcoord.toString());
+                    var freshmarker = marker1.clone();
+                    var markercoord = hex.worldcoord.clone();
+                    markercoord.y += 1;
+
+                    this.entity.addChild(freshmarker);
+                    freshmarker.setPosition(markercoord);
+                }
+            }, this);
+
+            // place obstacles at obstacle locations
+            _.forEach(obstacledata.obstacles, function(obstacle) {
+                var hex1coord = obstacle.hex1.hexcoord;
+                var hex2coord = obstacle.hex2.hexcoord;
+                var obstaclesegment = worldhexes.convertEdgeCoordinatesToWorldEndpoints([hex1coord, hex2coord]);
+                var midpoint = obstaclesegment[0].clone().add(obstaclesegment[1]).scale(0.5);
+                midpoint.y += 2;
+
+                var freshobstacle = baserock.clone();
+                this.entity.addChild(freshobstacle);
+                freshobstacle.setPosition(midpoint);
+                if (freshobstacle.rigidbody) {
+                    freshobstacle.rigidbody.syncEntityToBody();
+                }
+            }, this);
+            
+            // clear out exemplar objects
+            var exemplarcontainer = this.entity.findByName('exemplars');
+            exemplarcontainer.destroy();
+
+            // position the player at the start hex
+            var starthexes = worldhexes.findHexesWithTag('start');
+            var starthex = starthexes[0];
+            pc.log.write('Player start hex ' + starthex.hexcoord.toString() + ', world coord ' + starthex.worldcoord.toString());
+
+            var playerentity = this.entity.getRoot().findByName('Avatar');
+            playerentity.setPosition(starthex.worldcoord.x, starthex.worldcoord.y + 3, starthex.worldcoord.z);
+            playerentity.rigidbody.syncEntityToBody();
+        },
+
+        placehexes: function(worldhexes) {
+            var hexes = _.values(worldhexes.mapdata);
+            var hexobjectexemplar = this.entity.findByName('basichex');
+
             _.forEach(hexes, function(hex) {
                 var worldcoord = hex.worldcoord;
 
                 // nudge meshes up and down a little bit to avoid z-fighting of the flat ground
-                var perturbedworldcoord = new pc.Vec3(worldcoord.x, basehex.getPosition().y + Math.random() * 0.04 - 0.02, worldcoord.z);
+                var perturbedworldcoord = new pc.Vec3(worldcoord.x, hexobjectexemplar.getPosition().y + Math.random() * 0.04 - 0.02, worldcoord.z);
 
-                var freshhex = basehex.clone();
+                var freshhex = hexobjectexemplar.clone();
                 this.entity.addChild(freshhex);
                 freshhex.setPosition(perturbedworldcoord);
 
@@ -48,22 +97,17 @@ pc.script.create('worldmap', function (context) {
 
             }, this);
 
-            // place notations for the hexes (used for debugging)
-            _.forEach(hexes, function(hex) {
-                var markercoord = hex.worldcoord.clone();
-                markercoord.y += 1;
 
-                // mark spot with a region tag of some sort
-                if (_.some(hex.tags, function(tag) {return tag === 'reachable';})) {
-                    var freshmarker = marker1.clone();
-                    this.entity.addChild(freshmarker);
-                    freshmarker.setPosition(markercoord);
-                }
-            }, this);
+        },
+
+        placewalls: function(worldhexes) {
+            var hexes = _.values(worldhexes.mapdata);
+            var basetree = this.entity.findByName('basictree');
 
             // some hexes are adjacent but not "connected" so we have to place to wall
             // on the non-connected edge.
             var wallplacements = [];
+
             _.forEach(hexes, function(hex) {
                 var hexcoord = hex.hexcoord;
                 var adjhexcoords = Hexmaplib.getAdjacentHexCoordinates(hexcoord);
@@ -105,32 +149,7 @@ pc.script.create('worldmap', function (context) {
                 freshwall2.rigidbody.syncEntityToBody();
             }, this);
 
-            // place obstacles at obstacle locations
-            _.forEach(obstacledata.obstacles, function(obstacle) {
-                var hex1coord = obstacle.hex1.hexcoord;
-                var hex2coord = obstacle.hex2.hexcoord;
-                var obstaclesegment = worldhexes.convertEdgeCoordinatesToWorldEndpoints([hex1coord, hex2coord]);
-                var midpoint = obstaclesegment[0].clone().add(obstaclesegment[1]).scale(0.5);
-                midpoint.y += 2;
 
-                var freshobstacle = baserock.clone();
-                this.entity.addChild(freshobstacle);
-                freshobstacle.setPosition(midpoint);
-                if (freshobstacle.rigidbody) {
-                    freshobstacle.rigidbody.syncEntityToBody();
-                }
-            }, this);
-            
-            // clear out exemplar objects
-            var exemplarcontainer = this.entity.findByName('exemplars');
-            exemplarcontainer.destroy();
-
-            // position the player at the start hex
-            var starthexes = worldhexes.findHexesWithTag('start');
-            var starthex = starthexes[0];
-
-            var playerentity = this.entity.getRoot().findByName('Avatar');
-            playerentity.setPosition(starthex.worldcoord.x, starthex.worldcoord.y + 10, starthex.worldcoord.z);
         },
 
         // Called every frame, dt is time in seconds since last update
